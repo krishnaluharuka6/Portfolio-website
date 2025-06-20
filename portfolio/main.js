0529b683e2d58db80bcdbd0911343e912146b3cd
krishnaluharuka6 <rahdhekrishnaa@gmail.com>
2025-06-20 11:12:42 +0545

    Worked with navbar, home section and about section

---
+++ portfolio/main.js
@@ -0,0 +1,41 @@
+// const toggle = document.getElementById('toggleLight');
+// const body = document.querySelector('body');
+// const textlight = document.querySelectorAll('.text-light');
+// const nav = document.querySelector('.navbar');
+
+// toggle.addEventListener('click', function(){
+//     this.classList.toggle('bi-brightness-high-fill');
+//     // for (let i = 0; i < textdark.length; i++) {
+//     //     textdark[i].classList.toggle('text-light');
+//     // }
+//     if(this.classList.toggle('bi-moon')){
+//         body.style.background='#fff';
+//         body.style.color='#081b29';
+//         body.style.transaction='2s';
+//         nav.style.background='#fff';
+//         nav.style.color='#081b29';
+//         for (let i = 0; i < textlight.length; i++) {
+//             textlight[i].classList.add('text-dark');
+//             textlight[i].classList.remove('text-light');
+//         }
+//     }
+//     else{
+//         body.style.background='#081b29';
+//         body.style.color='white';
+//         body.style.transaction='2s';
+//         nav.style.background='#081b29';
+//         nav-style.color='white';       for (let i = 0; i < textlight.length; i++) {
+//             textlight[i].classList.remove('text-dark');
+//             textlight[i].classList.add('text-light');
+//         }
+//     }
+// })
+
+
+var typed= new Typed(".text",{
+    strings:["Web Developer","Curious Learner","Believer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
