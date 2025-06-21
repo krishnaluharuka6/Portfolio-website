437541a87ca752aed05f77dec7ea2c85b003169c
krishnaluharuka6 <rahdhekrishnaa@gmail.com>
2025-06-21 17:59:22 +0545

    Added Contact section, skills section, made responsive

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,39 +1,32 @@
-// const toggle = document.getElementById('toggleLight');
-// const body = document.querySelector('body');
-// const textlight = document.querySelectorAll('.text-light');
-// const nav = document.querySelector('.navbar');
+const toggle = document.getElementById('toggleLight');
+const body = document.querySelector('body');
 
-// toggle.addEventListener('click', function(){
-//     this.classList.toggle('bi-brightness-high-fill');
-//     // for (let i = 0; i < textdark.length; i++) {
-//     //     textdark[i].classList.toggle('text-light');
-//     // }
-//     if(this.classList.toggle('bi-moon')){
-//         body.style.background='#fff';
-//         body.style.color='#081b29';
-//         body.style.transaction='2s';
-//         nav.style.background='#fff';
-//         nav.style.color='#081b29';
-//         for (let i = 0; i < textlight.length; i++) {
-//             textlight[i].classList.add('text-dark');
-//             textlight[i].classList.remove('text-light');
-//         }
-//     }
-//     else{
-//         body.style.background='#081b29';
-//         body.style.color='white';
-//         body.style.transaction='2s';
-//         nav.style.background='#081b29';
-//         nav-style.color='white';       for (let i = 0; i < textlight.length; i++) {
-//             textlight[i].classList.remove('text-dark');
-//             textlight[i].classList.add('text-light');
-//         }
-//     }
-// })
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-brightness-high-fill');
+    if(this.classList.toggle('bi-moon')){
+        body.style.background='#e3f2fd';
+        body.style.color='#0e3653';
+        body.style.transition='2s';
+    }
+    else{
+        body.style.background='#0e3653';
+        body.style.color='#e3f2fd';
+        body.style.transition='2s';
+    }
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("preloader").style.display = "none";
+    body.style.background = '#0e3653';
+    body.style.color = '#e3f2fd';
+    // body.style.transition = '2s';
+});
+
+
 
 
 var typed= new Typed(".text",{
-    strings:["Web Developer","Curious Learner","Believer"],
+    strings:["Web Developer","Curious Learner","Listener"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
