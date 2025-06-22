6a1598e02f75f032b52a42338cd7e0b412856338
krishnaluharuka6 <krishna.luharuka11@gmail.com>
2025-06-22 18:49:59 +0545

    made responsive

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,6 @@
 const toggle = document.getElementById('toggleLight');
 const body = document.querySelector('body');
+const navbar = document.querySelector('.navbar');
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-brightness-high-fill');
@@ -7,11 +8,13 @@ toggle.addEventListener('click', function(){
         body.style.background='#e3f2fd';
         body.style.color='#0e3653';
         body.style.transition='2s';
+        navbar.style.background='#e3f2fd';
     }
     else{
         body.style.background='#0e3653';
         body.style.color='#e3f2fd';
         body.style.transition='2s';
+        navbar.style.background='#0e3653';
     }
 });
 
@@ -19,14 +22,15 @@ window.addEventListener("DOMContentLoaded", function() {
     document.getElementById("preloader").style.display = "none";
     body.style.background = '#0e3653';
     body.style.color = '#e3f2fd';
-    // body.style.transition = '2s';
+    navbar.style.background='#0e3653';
+    body.style.transition = '2s';
 });
 
 
 
 
 var typed= new Typed(".text",{
-    strings:["Web Developer","Curious Learner","Listener"],
+    strings:["Web Developer","Curious Learner","Problem Solver"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
